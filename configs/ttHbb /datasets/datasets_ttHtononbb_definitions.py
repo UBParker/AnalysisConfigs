5511569537992ac887823036ea36c36f832091c9
Ashley Marie Parker <ash.marie.parker@gmail.com>
2023-06-30 12:16:54 +0200

    Create datasets_ttHtononbb_definitions.py

---
+++ configs/ttHbb /datasets/datasets_ttHtononbb_definitions.py
@@ -0,0 +1,21 @@
+{
+    "ttHToNonbb_Powheg_2018": {
+	"sample": "ttHToNonbb",
+	"json_output": "datasets/backgrounds_MC_TTHtoNonbb_redirector.json",
+	"files": [
+	    {
+		"das_names": [
+		    "/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"
+		],
+		"metadata": {
+		    "year": "2018",
+		    "isMC": true,
+		    "xsec": "0.2118"
+		},
+		"das_parents_names": [
+		    "/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM"
+		]
+	    }
+	]
+    }
+}
